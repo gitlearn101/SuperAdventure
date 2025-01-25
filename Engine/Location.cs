4b353341dcf06c6d72ecb3e5d21eef9e87f53822
gitlearn101 <anand.learn101@gmail.com>
2025-01-25 09:39:49 +0530

    Coding till Lesson 09.1 done.

--- Engine/Location.cs
+++ Engine/Location.cs
@@ -11,13 +11,24 @@ namespace Engine
         public int ID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public Item ItemRequiredToEnter { get; set; }
+        public Quest QuestAvailableHere { get; set; }
+        public Monster MonsterLivingHere { get; set; }
+        public Location LocationToNorth { get; set; }
+        public Location LocationToEast { get; set; }
+        public Location LocationToSouth { get; set; }
+        public Location LocationToWest { get; set; }
 
 
-        public Location(int id, String name, String description) 
+        public Location(int id, String name, String description, Item itemRequiredToEnter = null,
+            Quest questAvailableHere = null,Monster monsterLivingHere = null) 
         {
             ID = id;
             Name = name;
             Description = description;
+            itemRequiredToEnter = ItemRequiredToEnter;
+            questAvailableHere = QuestAvailableHere;
+            monsterLivingHere = MonsterLivingHere;
 
         }
     }
--- SuperAdventure/SuperAdventure.cs
+++ SuperAdventure/SuperAdventure.cs
@@ -23,22 +23,8 @@ namespace SuperAdventure
             
             Location location = new Location(1, "Home", "This is your house.");
 
-            /* to reduce lines we use parameters in the constructor as done above
-            location.ID = 1;
-            location.Name = "Home";
-            location.Description = "This is your house.";
-            */
-
+            _player = new Player(10, 10, 20, 0, 1);
             
-
-            _player = new Player();
-
-            _player.CurrentHitPoints = 10;
-            _player.MaximumHitPoints = 10;
-            _player.Gold = 20;
-            _player.ExperiencePoints = 0;
-            _player.Level = 1;
-
             lblHitPoints.Text = _player.CurrentHitPoints.ToString();
             lblGold.Text = _player.Gold.ToString();
             lblExperience.Text = _player.ExperiencePoints.ToString();
